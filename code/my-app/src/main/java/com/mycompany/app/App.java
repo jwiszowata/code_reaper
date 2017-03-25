106ab945cf6840023d7080a1f1999aad191db712
Joanna <wiszowata.joanna@gmail.com>
2017-03-25 15:59:59 +0100

    Division of file on functions

--- code/my-app/src/main/java/com/mycompany/app/App.java
+++ code/my-app/src/main/java/com/mycompany/app/App.java
@@ -1,20 +1,60 @@
 package com.mycompany.app;
+
 import java.io.*;
+import java.util.*;
 import com.github.javaparser.ast.*;
 import com.github.javaparser.*;
-/**
- * Hello world!
- *
- */
+import com.github.javaparser.ast.visitor.*;
+import com.github.javaparser.ast.body.*;
+import com.github.javaparser.ast.comments.*;
+import com.github.javaparser.ast.expr.*;
+
 public class App {
+
     public static void main(String[] args) throws Exception {
-        // creates an input stream for the file to be parsed
-        FileInputStream in = new FileInputStream("test.java");
+        FileInputStream in = new FileInputStream("test1.java");
 
-        // parse the file
         CompilationUnit cu = JavaParser.parse(in);
+        removeComments(cu);
+        new MethodVisitor().visit(cu, "test1");
+    }
 
-        // prints the resulting compilation unit to default system output
-        System.out.println(cu.toString());
+    private static void removeComments(CompilationUnit cu) {
+        List<Comment> commentList = cu.getComments();
+        Iterator<Comment> it = commentList.iterator();
+        while (it.hasNext()) {
+            Comment c = it.next();
+            c.remove();
+        }
+    }
+
+    /**
+     * Simple visitor implementation for visiting MethodDeclaration nodes.
+     */
+    private static class MethodVisitor extends VoidVisitorAdapter<String> {
+        @Override
+        public void visit(MethodDeclaration method, String filename) {
+
+            removeAnnotations(method);
+            String path = getOutputPath(method, filename);
+
+            try (Writer writer = new BufferedWriter(new OutputStreamWriter(
+                                                            new FileOutputStream(path), "utf-8"))) {
+                writer.write(method.toString());
+            } catch (IOException e) {
+                System.out.println("Got something wrong.");
+            }
+            System.out.println(path + " done");
+            super.visit(method, filename);
+        }
+
+        private static void removeAnnotations(MethodDeclaration m) {
+            m.setAnnotations(new NodeList<AnnotationExpr>());
+        }
+
+        private static String getOutputPath(MethodDeclaration method, String filename) {
+            String funName = method.getName().toString();
+            return "./functions/" + filename + "_" + funName;
+        }
     }
 }
