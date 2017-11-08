0c1cb11b43ec7fb7b7062613d28b59fe1b9fd9cf
Joanna Wiszowata <wiszowata.joanna@gmail.com>
2017-11-08 20:17:01 +0100

    Timer added to task screen. #6

--- code_reaper_app/code_reaper_site/code_reaper/static/code_reaper/script.js
+++ code_reaper_app/code_reaper_site/code_reaper/static/code_reaper/script.js
@@ -1,31 +1,30 @@
 $(document).ready(function () {
 
+    var nIntervId;
 
+    function addCheckboxes() {
+        $("td.gutter > div.line").each(function() {
+            $(this).prepend("<input class=\"check-input\" type=\"checkbox\" value=\"\" checked>");
+        });
 
-    $("button.start").click(function(){
-
-            $("td.gutter > div.line").each(function() {
-                $(this).prepend("<input class=\"check-input\" type=\"checkbox\" value=\"\" checked>");
+        $('input.check-input').click(function() {
+            let parent = $(this).parent()[0];
+            let divName = changeToDescription(parent);
+            $("div.container > " + divName + " > code").each(function() {
+                $(this).toggleClass("gray");
             });
+        });
 
-            $('input.check-input').click(function() {
-                let parent = $(this).parent()[0];
-                let divName = changeToDescription(parent);
-                $("div.container > " + divName + " > code").each(function() {
-                    $(this).toggleClass("gray");
-                });
-            });
+        //$("div.container > div.line").each(function() {
+        //    $(this).append("<code class=\"java comments\" style=\"display: none\">//" + getText($(this)) + "</code>");
+        //});
 
-            //$("div.container > div.line").each(function() {
-            //    $(this).append("<code class=\"java comments\" style=\"display: none\">//" + getText($(this)) + "</code>");
-            //});
-
-            //$("div.container > div.line").click(function() {
-            //    $(this).children().each(function() {
-            //        $(this).toggle();
-            //    });
-            //});
-    });
+        //$("div.container > div.line").click(function() {
+        //    $(this).children().each(function() {
+        //        $(this).toggle();
+        //    });
+        //});
+    }
 
     /*function getText(obj) {
         var s = '';
@@ -42,4 +41,31 @@ $(document).ready(function () {
         }
         return s;
     }
+
+    function startTimer(duration, display) {
+        var timer = duration, minutes, seconds;
+        nIntervId = setInterval(function () {
+            minutes = parseInt(timer / 60, 10)
+            seconds = parseInt(timer % 60, 10);
+
+            minutes = minutes < 10 ? "0" + minutes : minutes;
+            seconds = seconds < 10 ? "0" + seconds : seconds;
+
+            display.textContent = minutes + ":" + seconds;
+
+            if (--timer < 0) {
+                clearInterval(nIntervId);
+                $('#finishModal').modal('show');
+            }
+        }, 1000);
+    }
+
+    $('#startModal').modal('show');
+
+    $("button.start").click(function() {
+        var time = 60 * 2,
+            display = document.querySelector('#time');
+        startTimer(time, display);
+        addCheckboxes();
+    });
 });
