b7cff6e993182dbc107f41df246b12d65cc28333
Joanna Wiszowata <wiszowata.joanna@gmail.com>
2018-03-29 21:11:34 +0200

    font change

---
+++ code_reaper_site/static/code_reaper/login.js
@@ -0,0 +1,6 @@
+$(document).ready(function () {
+	$("label[for=id_username]").text("Login:");
+	$("label[for=id_password]").text("Hasło:");
+	$("label[for=id_password1]").text("Hasło:");
+	$("label[for=id_password2]").text("Powtórz hasło:");
+});
