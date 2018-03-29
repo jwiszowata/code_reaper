c19083fc780d5d198da4be45d680f42de3d32e0a
Joanna Wiszowata <wiszowata.joanna@gmail.com>
2018-03-29 15:58:33 +0200

    Translation of form

--- code_reaper/static/code_reaper/login.js
+++ code_reaper/static/code_reaper/login.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
-	$('#nav-login > form > p:nth-child(3) > label').text = "Login";
-	$('#nav-login > form > p:nth-child(4) > label').text = "Hasło";
-	$('#nav-register > form > p:nth-child(2) > label').text = "Login";
-	$('#nav-register > form > p:nth-child(3) > label').text = "Hasło";
-	$('#nav-register > form > p:nth-child(6) > label').text = "Powtórz hasło";
+	$('#nav-login > form > p:nth-child(3) > label').text("Login");
+	$('#nav-login > form > p:nth-child(4) > label').text("Hasło");
+	$('#nav-register > form > p:nth-child(2) > label').text("Login");
+	$('#nav-register > form > p:nth-child(3) > label').text("Hasło");
+	$('#nav-register > form > p:nth-child(6) > label').text("Powtórz hasło");
 });
