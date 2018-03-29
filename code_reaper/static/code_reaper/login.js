93df064561c4a9dac14754580d2c083471cd5431
Joanna Wiszowata <wiszowata.joanna@gmail.com>
2018-03-29 16:31:07 +0200

    Login template

--- code_reaper/static/code_reaper/login.js
+++ code_reaper/static/code_reaper/login.js
@@ -1,7 +1,6 @@
 $(document).ready(function () {
-	$('#nav-login > form > p:nth-child(3) > label').text("Login");
-	$('#nav-login > form > p:nth-child(4) > label').text("Hasło");
-	$('#nav-register > form > p:nth-child(2) > label').text("Login");
-	$('#nav-register > form > p:nth-child(3) > label').text("Hasło");
-	$('#nav-register > form > p:nth-child(6) > label').text("Powtórz hasło");
+	$("label[for=id_username]").text("Login:");
+	$("label[for=id_password]").text("Hasło:");
+	$("label[for=id_password1]").text("Hasło:");
+	$("label[for=id_password2]").text("Powtórz hasło:");
 });
